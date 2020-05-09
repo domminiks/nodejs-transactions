0f39e698c99464d7f36f59029d5d32bc7629d1c8
Pedro Felipe Dominguite <pfdominguite@gmail.com>
2020-05-09 16:31:21 -0300

    Added service to create transactions

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,48 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
 
+import AppError from '../errors/AppError';
 import Transaction from '../models/Transaction';
+import TransactionType from '../models/enums/TransactionTypeEnum';
+import CreateCategoryService from './CreateCategoryService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: TransactionType;
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    if (!Object.values(TransactionType).includes(type)) {
+      throw new AppError("Invalid 'type' for request.", 400);
+    }
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const { total } = await transactionsRepository.getBalance();
+
+    if (type === TransactionType.OUTCOME && total < value) {
+      throw new AppError('Insufficient funds.');
+    }
+
+    const createCategory = new CreateCategoryService();
+    const newCategory = await createCategory.execute({
+      title: category,
+    });
+
+    const transaction = transactionsRepository.create({
+      title,
+      value: Math.abs(Number(value)),
+      type,
+      category: newCategory,
+    });
+    await transactionsRepository.save(transaction);
+    return transaction;
   }
 }
 
